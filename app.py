3e6e37eca631647da82b8d6575d0148a49a6b30d
nishuu09 <danishabyaz1209@gmail.com>
2025-07-18 16:05:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,1 @@
-git init
-git add .
-git commit -m "upload streamlit app"
-git branch -M main
-git remote add origin https://github.com/username/nama-repo.git
-git push -u origin main
+import streamlit as st
