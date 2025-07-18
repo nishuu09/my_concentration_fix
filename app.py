e1284d550bfa518a5e6f9cccbd32a451ed6e7f20
nishuu09 <danishabyaz1209@gmail.com>
2025-07-18 16:31:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ st.sidebar.title("🔍 Navigasi")
 menu = st.sidebar.radio("Pilih halaman:", ["Beranda", "Konversi", "Tentang"])
 
 if menu == "Beranda":
-    st.write("Oke")
+    st.title("MY CALCULATOR")
+    st.write("Aplikasi Pengkonversi Konsentrasi Kimia")
 elif menu == "Konversi":
     st.write("Silakan konversi data kimia.")
 elif menu == "Tentang":
     st.write("Aplikasi ini dibuat untuk praktikum kimia.")
 
 #===========================================================================================
-st.title("MY CALCULATOR")
