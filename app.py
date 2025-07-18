08d1629a9d1960d49088558289a83bbf99d36836
nishuu09 <danishabyaz1209@gmail.com>
2025-07-18 16:02:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+def hitung_massa(mol, massa_molar):
+    massa = mol * massa_molar
+    return massa
+
+# Contoh penggunaan:
+mol = 2  # mol
+massa_molar = 18  # g/mol (contoh: H2O)
+
+massa = hitung_massa(mol, massa_molar)
+print(f"Massa: {massa} gram")
 
