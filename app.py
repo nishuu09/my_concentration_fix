9b9aacc6bf3574b09781744d446279370fe340f0
nishuu09 <danishabyaz1209@gmail.com>
2025-07-18 16:17:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
-if st.button("Hitung"):
-    st.success(f"Hasil: {angka * 2} ({pilihan})")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.write("Input Kiri")
+    st.number_input("Mol", key="mol")
+
+with col2:
+    st.write("Input Kanan")
+    st.number_input("Gram", key="gram")
+
